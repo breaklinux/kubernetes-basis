14bad563d91b8ddd22c2fa8d582bf745b8d7f506
breaklinux <13164278484@163.com>
2020-10-28 17:36:31 +0800

    Update query-https-secret.sh

--- k8s-install-ingress/k8s-ingress-exmple/ops-py-flask-api/nginx-ingress-https/query-https-secret.sh
+++ k8s-install-ingress/k8s-ingress-exmple/ops-py-flask-api/nginx-ingress-https/query-https-secret.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#注入cecret
+#证书认证注入管理员权限
+for ns in `kubectl get namespaces|grep -v "NAME"|awk '{print $1}'`;do kubectl create secret tls letsencrypt-https --key ./certbot/config-dir/live/xiaolige.com/privkey.pem  --cert ./certbot/config-dir/live/xiaolige.com/fullchain.pem -n $ns; done
+ 
+
 #查询secret 证书详细信息
 echo $(kubectl get secret letsencrypt-xiaolige-site  -n devops-python -o jsonpath='{.data.tls\.crt}')| base64 -d|openssl x509 -text -noout  
